e185a53759b464b2252de7de3cbaf28421be2d98
Zoltan Burgermeiszter <5986229+zburgermeiszter@users.noreply.github.com>
2025-09-30 16:28:34 +0100

    Feature/GitHub Action (#1)
    * pipeline test
    
    * install asdf in pipeline
    
    * checkout
    
    * env vars configured for OIDC
    
    * terragrunt init/plan
    
    * cd terraform
    
    * list files
    
    * cd fix
    
    * workddir fix
    
    * path fix
    
    * tree
    
    * terragrunt path and plan fix
    
    * Build complete
    
    * bun ci fix
    
    * artifact restore fix
    
    * workdir fix
    
    * test runner added to pipeline
    
    * EC2 type+AMI change
    
    * Pipeline changes
    
    * mock AWS_REGION
    
    * terragrunt validate
    
    * ec2 tf cleanup
    
    * terragrunt apply for main branch only

--- terraform/modules/ec2-inspector/ec2.tf
+++ terraform/modules/ec2-inspector/ec2.tf
@@ -1,20 +1,7 @@
-data "aws_ami" "this" {
-  most_recent = true
-  owners      = ["amazon"]
-  filter {
-    name   = "architecture"
-    values = ["arm64"]
-  }
-  filter {
-    name   = "name"
-    values = ["al2023-ami-2023*"]
-  }
-}
-
 resource "aws_instance" "instance" {
   count         = 2
-  ami           = data.aws_ami.this.id
-  instance_type = "t4g.nano"
+  ami           = "ami-01dad14554d9a67f4" # Ubuntu 24.04 LTS
+  instance_type = "t2.micro"
   tags = {
     Name = "test-instance-${count.index}"
   }

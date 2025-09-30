6f38de574d56a99fc88bdb44265e4ebf204725b3
Zoltan Burgermeiszter <zoltan@burgermeiszter.com>
2025-09-30 16:22:27 +0100

    ec2 tf cleanup

--- terraform/modules/ec2-inspector/ec2.tf
+++ terraform/modules/ec2-inspector/ec2.tf
@@ -1,19 +1,5 @@
-# data "aws_ami" "this" {
-#   most_recent = true
-#   owners      = ["amazon"]
-#   filter {
-#     name   = "architecture"
-#     values = ["arm64"]
-#   }
-#   filter {
-#     name   = "name"
-#     values = ["al2023-ami-2023*"]
-#   }
-# }
-
 resource "aws_instance" "instance" {
-  count = 2
-  # ami           = data.aws_ami.this.id
+  count         = 2
   ami           = "ami-01dad14554d9a67f4" # Ubuntu 24.04 LTS
   instance_type = "t2.micro"
   tags = {
